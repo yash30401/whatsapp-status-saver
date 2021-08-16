fe541ad1345c6cee73bb9a076dd9063e21b4d061
yash30401 <yashveersingh30401@gmail.com>
2021-08-16 21:35:57 +0530

    Dialog Of menu Created.

--- app/src/main/java/com/dynamichub/yash/whatsappstatussaver/MainActivity.java
+++ app/src/main/java/com/dynamichub/yash/whatsappstatussaver/MainActivity.java
@@ -1,14 +1,51 @@
 package com.dynamichub.yash.whatsappstatussaver;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    LinearLayout layoutMenuicon;
+    Dialog dialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layoutMenuicon=findViewById(R.id.layout_menu_icon);
+        dialog=new Dialog(this);
+
+        layoutMenuicon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMenu();
+            }
+        });
+
+
+    }
+
+    private void openMenu() {
+        //This Function Opens The Menu
+        dialog.setContentView(R.layout.menu_layout);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().setGravity(Gravity.START);
+        dialog.getWindow().setLayout(920, WindowManager.LayoutParams.MATCH_PARENT);
+
+        dialog.show();
     }
 }
--- app/src/main/java/com/dynamichub/yash/whatsappstatussaver/SplashScreen.java
+++ app/src/main/java/com/dynamichub/yash/whatsappstatussaver/SplashScreen.java
@@ -2,10 +2,14 @@ package com.dynamichub.yash.whatsappstatussaver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
+
+    final static int SPLASH_TIMER=2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +17,16 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIMER);
 
     }
 }
